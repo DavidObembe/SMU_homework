602e9624be5884f25c00f03d78b00e0c3c9fee8a
DavidObembe <daveobembe@yahoo.co.uk>
2020-08-13 17:24:05 -0500

    completed mapwork

--- solutions/17-Mapping-Web/static/js/logic.js
+++ solutions/17-Mapping-Web/static/js/logic.js
@@ -1,11 +1,17 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson",
+    boundaryURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
+
 
 // Perform a GET request to the query URL
+
 d3.json(queryUrl, function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
     console.log(data.features);
     createFeatures(data.features);
+
+
 
 });
 
@@ -30,6 +36,18 @@ function createFeatures(earthquakeData) {
         }
         return color;
     }
+    // function getCircleColor(mag) {
+    //     return mag > 4 ? '#800026' :
+    //         mag > 5 ? '#BD0026' :
+    //         mag > 6 ? '#E31A1C' :
+    //         mag > 7 ? '#FC4E2A' :
+    //         mag > 8 ? '#FD8D3C' :
+    //         mag > 9 ? '#FEB24C' :
+    //         mag > 10 ? '#FED976' :
+    //         '#FFEDA0';
+    // }
+
+
 
     function getMarkerSize(points) {
         return points ** 1.8;
@@ -45,6 +63,8 @@ function createFeatures(earthquakeData) {
         onEachFeature: onEachFeature
             // style: style
     });
+
+    ///legend
 
     console.log(earthquakeData);
 
@@ -71,15 +91,44 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-v9",
+        accessToken: API_KEY
+    });
+
+
+
+
+    //boundary layer for tectonic plates are added here
+    var boundarylayer_style = {
+        "color": "#C0392B",
+        "weight": 1,
+        "opacity": 0.9
+    };
+
+    let bLayer;
+
+    d3.json(boundaryURL, function(d) {
+        bLayer = L.geoJSON(d.features, { style: boundarylayer_style }).addTo(myMap);
+
+    });
+
+
+
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
         "Street Map": streetmap,
-        "Dark Map": darkmap
+        "Dark Map": darkmap,
+        "satellite -v9": satellite
     };
 
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
-        Earthquakes: earthquakes
+        Earthquakes: earthquakes,
+        // Tectonic_boundaries: bLayer
+
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -87,7 +136,7 @@ function createMap(earthquakes) {
         center: [
             37.09, -95.71
         ],
-        zoom: 5,
+        zoom: 3,
         layers: [streetmap, earthquakes]
     });
 
@@ -103,6 +152,49 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+
+
+    //legend function= it is the same as the circlecolor function used at the top of the page.
+    function getCircleColor(mag) {
+        let color = "";
+        if (mag < 5) {
+            color = "yellow";
+        } else if (mag < 6.5) {
+            color = "orange";
+        } else if (mag < 10.0) {
+            color = "red";
+        }
+        return color;
+    }
+
+    //legend is added here
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function(map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            // categories = ["less than 5", "less than 6.5", "less than 10"],
+            categories = [0, 5, 6.5, 10],
+            labels = ['<strong>Categories</strong>'];
+
+        // loop through our earthquak intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < categories.length; i++) {
+            div.innerHTML +=
+                // '<i style="background:' + getCircleColor(grades[i] + 1) + '"></i> ' +
+                // grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+                labels.push(
+                    '<i class="circle" style="background:' + getCircleColor(categories[i]) + '"></i> ' +
+                    // (categories[i] ? categories[i] : '+'));
+                    categories[i] + (categories[i + 1] ? '&ndash;' + categories[i + 1] + '<br>' : '+'));
+        }
+        div.innerHTML = labels.join('<br>');
+        return div;
+
+    };
+
+    legend.addTo(myMap);
+
 
 
 }
